794ad271964b4023967b587ce7ba6601b9636771
E. O² <78842550+proftosin7@users.noreply.github.com>
2022-08-20 18:51:18 +0100

    Edit1

--- src/components/CheckoutForm/FormInput.js
+++ src/components/CheckoutForm/FormInput.js
@@ -2,7 +2,7 @@ import React from "react";
 import { TextField, Grid } from "@mui/material";
 import { useFormContext, Controller } from "react-hook-form";
 
-const FormInput = ({ name, label, required }) => {
+const FormInput = ({ name, label }) => {
   const { control } = useFormContext();
   return (
     <Grid item xs={12} sm={6}>
@@ -11,13 +11,7 @@ const FormInput = ({ name, label, required }) => {
         control={control}
         name={name}
         render={({ field }) => (
-          <TextField
-            {...field}
-            name={name}
-            label={label}
-            required={required}
-            fullWidth
-          />
+          <TextField {...field} name={name} label={label} required fullWidth />
         )}
       />
     </Grid>
